78fec543d7aa907a44bdb5cb95e5c447c5f26b07
Yifan Shi <stevenls1118@msn.com>
2022-10-06 18:06:00 -0600

    sum numbers in array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach(i => {
+    if (Array.isArray(i)) {
+      sum += sumItems(i);
+    } else {
+      sum += i;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
